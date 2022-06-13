eb7fb6173b027728919b1e31f17758ca72ef42c2
ramkumar <ramviru9@gmail.com>
2022-06-13 23:06:36 +0530

    commit

--- src/Gallary.js
+++ src/Gallary.js
@@ -2,53 +2,73 @@ import react from "react";
 function Gallary(){
     return(
         <div className="container-fluid">
-            <div className="row mt-3">
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuoKtHFSIlZUgqHJyQKmTdxYSo4FDM0Ejq6A&usqp=CAU"></img>
+            <div class="grid-container">
+                {/* <div class="grid-item">
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuoKtHFSIlZUgqHJyQKmTdxYSo4FDM0Ejq6A&usqp=CAU"></img> */}
+                {/* <div class="top-layer">
+                    <button class="btn btn-danger btn-small rounded-pill">Save</button>
+                        <div class="top-icons ms-auto">
+                            <a class="nav-link text-dark" href="#"><i class="bi bi-upload"></i></a>
+                            <a class="nav-link text-dark" href="#"><i class="bi bi-three-dots"></i></a>
+                    </div>
+                </div> */}
+                {/* </div>
+                 */}
+                
+            <div class="grid-item">
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlcycW7xLwycb8llHsIxGzEkyPgk-K_7y0w&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycH6jh5yj4agzRAA76H5yahKQeL2a0Tp7Jw&usqp=CAU"></img>
+                <p>Superheroes</p>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgKgsZ1bpawOkbDvIqQqxGyNytSatEBytAcg&usqp=CAU"></img>
+            <img className="img-fluid img-size" src="https://i.pinimg.com/236x/2d/5e/5a/2d5e5a14cd120fbd21969f55eef3be18.jpg"></img>
                 </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlcycW7xLwycb8llHsIxGzEkyPgk-K_7y0w&usqp=CAU"></img>
+                <div class="grid-item">
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycH6jh5yj4agzRAA76H5yahKQeL2a0Tp7Jw&usqp=CAU"></img>
+                <p>Superheroes</p>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycH6jh5yj4agzRAA76H5yahKQeL2a0Tp7Jw&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlcycW7xLwycb8llHsIxGzEkyPgk-K_7y0w&usqp=CAU"></img>
+                <p>Superheroes</p>
+            <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycH6jh5yj4agzRAA76H5yahKQeL2a0Tp7Jw&usqp=CAU"></img>
                 </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycH6jh5yj4agzRAA76H5yahKQeL2a0Tp7Jw&usqp=CAU"></img>
+                <div class="grid-item">
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgKgsZ1bpawOkbDvIqQqxGyNytSatEBytAcg&usqp=CAU"></img>
+                <p>Superheroes</p>
+                <img className="img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkTm3ifvlKPa7ZL-q_evaMZz1irQTOOgWM6g&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYSAZE_w2RVaA7CR3RYANJCbmVfMnsZ78ACg&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://i.pinimg.com/236x/2d/5e/5a/2d5e5a14cd120fbd21969f55eef3be18.jpg"></img>
+                <p>Superheroes</p>
                 </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBymQ4le8mauk-UPIcG4OBq6AIZyKly2o6YQ&usqp=CAU"></img>
+                <div class="grid-item">
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYSAZE_w2RVaA7CR3RYANJCbmVfMnsZ78ACg&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycH6jh5yj4agzRAA76H5yahKQeL2a0Tp7Jw&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlcycW7xLwycb8llHsIxGzEkyPgk-K_7y0w&usqp=CAU"></img>
+                <img className="img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkTm3ifvlKPa7ZL-q_evaMZz1irQTOOgWM6g&usqp=CAU"></img>
                 </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBfXezgUayX6kmyTw9VxkSqSUPlKz8ubQcrg&usqp=CAU"></img>
+                <div class="grid-item">
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYSAZE_w2RVaA7CR3RYANJCbmVfMnsZ78ACg&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRycH6jh5yj4agzRAA76H5yahKQeL2a0Tp7Jw&usqp=CAU"></img>
+                <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlcycW7xLwycb8llHsIxGzEkyPgk-K_7y0w&usqp=CAU"></img>
+                <img className="img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkTm3ifvlKPa7ZL-q_evaMZz1irQTOOgWM6g&usqp=CAU"></img>
                 </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLVDNE4eVwxTF4-qZGFG6f-pGJ0cAmV50ANw&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlcycW7xLwycb8llHsIxGzEkyPgk-K_7y0w&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4OVb85HA0e4BOdbAum-SPQHzP5Z6wRtCZBA&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXJJQRBc-J6ft-hhCXWcaPR_ToayMY_WXxog&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo9GNAUQu1VLWB89DtmUrBsG4WGI4YKf3HjQ&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrF3Wll01YmKfj02QQ3-FeMd691zDEkEH7cw&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmlcycW7xLwycb8llHsIxGzEkyPgk-K_7y0w&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYSAZE_w2RVaA7CR3RYANJCbmVfMnsZ78ACg&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid img-size" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgKgsZ1bpawOkbDvIqQqxGyNytSatEBytAcg&usqp=CAU"></img>
-                </div>
-                <div className="col-lg-3">
-                    <img className="img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkTm3ifvlKPa7ZL-q_evaMZz1irQTOOgWM6g&usqp=CAU"></img>
-                </div>
+            
             </div>
+            {/* <div className="row mt-3 ms-3 me-3">
+                <div class="image">
+                    <img class="" src="https://i.pinimg.com/236x/75/ca/de/75cade873affd2df73e9d497cf6f0957.jpg"></img>
+                <div class="">
+                    <button class="btn btn-danger btn-sm rounded-pill image-btn">Save</button>
+                <div class="image-icons">
+                    <a class="icon-link text-dark" href="#"><i class="bi bi-upload"></i></a>
+                    <a class="icon-link text-dark" href="#"><i class="bi bi-three-dots"></i></a>
+                <div class="content">
+                    <p>MS Dhoni</p>
+                    <img class="img-fluid" src="https://i.pinimg.com/75x75_RS/58/3b/b4/583bb46c61597c0215269796cddb138e.jpg"></img>
+                    <p>MSD</p>
+                </div>
+                </div>
+                </div>
+                </div>
+            </div> */}
         </div>
     )
 }
